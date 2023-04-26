855ade92c3a4f25399be1b994dc6d3bdb7e2c746
Nguyen Tri Tai <nguyentritai210804@gmail.com>
2023-04-26 12:36:45 +0700

    video12

--- src/configs/connectDB.js
+++ src/configs/connectDB.js
@@ -1,10 +1,15 @@
-import mysql from "mysql2/promise";
+import mysql from 'mysql2/promise';
 
-// create the pool to database
+// // create the connection to database
+
+console.log("Creating connection pool...");
+
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  database: "nodejsbasic",
-});
+    host: 'localhost',
+    user: 'root',
+    database: 'nodejsbasic',
+    // password: 'password'
+})
+
 
 export default pool;
